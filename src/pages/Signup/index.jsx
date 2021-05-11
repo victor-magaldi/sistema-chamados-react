a6ffd51ce236ed221615d04f9fa2d1920e02da00
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-05-11 20:03:30 -0300

    cria pagina de registro

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/logo.png";
+import styles from "./Signup.module.css";
+
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  function handleSubmit() {
+    alert("submetido");
+  }
   return (
-    <div className="App">
-      <h1>Cadastro</h1>
+    <div className={styles.registerContainer}>
+      <div className={styles.register}>
+        <div className={styles.registerArea}>
+          <img src={logo} alt="logo-register" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Registrar</h1>
+          <input
+            type="text"
+            placeholder="nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar Conta</button>
+        </form>
+        <Link to="/" className={styles.linkCreateAccount}>
+          Já tem uma conta?
+        </Link>
+      </div>
     </div>
   );
 }
