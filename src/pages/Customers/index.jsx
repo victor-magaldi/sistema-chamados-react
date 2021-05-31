6b25a99b3c650a06c5c07df117ff0f0139e070b9
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-05-31 19:56:32 -0300

    cria jsx form customers

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -1,17 +1,58 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/Auth";
 import styles from "./Customers.module.css";
 
+import Title from "../../components/Title";
 import Header from "../../components/Header";
 
+import { FiUser } from "react-icons/fi";
+
 export default function Customers() {
-  const { signOut } = useContext(AuthContext);
-  console.log(signOut);
+  const {} = useContext(AuthContext);
+  const [nameFantasy, setNameFantasy] = useState("");
+  const [cnpj, setCnpj] = useState("");
+  const [address, setAddres] = useState("");
+
+  async function handleSave() {}
+
   return (
     <>
       <Header />
       <div className={styles.containerCustomers}>
         <h2>Página de Clientes</h2>
+        <Title name="Clientes">
+          <FiUser size={25} />
+        </Title>
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={handleSave}>
+            <label htmlFor="nameFantasy">nome</label>
+            <input
+              type="text"
+              id="nameFantasy"
+              placeholder="nome da sua empresa"
+              value={nameFantasy}
+              onChange={(e) => setNameFantasy(e.target.value)}
+            />
+
+            <label htmlFor="cnpj">cnpj</label>
+            <input
+              type="cnpj"
+              id="cnpj"
+              value={cnpj}
+              onChange={(e) => setCnpj(e.target.value)}
+            />
+
+            <label htmlFor="adress">Endereço</label>
+            <input
+              type="text"
+              id="adress"
+              value={address}
+              onChange={(e) => setAddres(e.target.value)}
+            />
+
+            <button type="submit">Salvar</button>
+          </form>
+        </div>
       </div>
     </>
   );
