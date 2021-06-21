2199eac9eed7823073ef3d4fc845eeeffbe3a807
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-06-21 19:49:45 -0300

    tela dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,15 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../contexts/Auth";
+import styles from "./Dashboard.module.css";
+
+import { FiMessageSquare, FiPlus } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 import Header from "../../components/Header";
+import Title from "../../components/Title";
 
 export default function Dashboard() {
+  const [calls, setCalls] = useState([]);
+  console.log(setCalls);
   const { signOut } = useContext(AuthContext);
   return (
     <>
       <Header />
-      <div className="containerDashboard">
-        <h3>Dashboard</h3>
+      <div className={styles.containerDashboard}>
+        <div className={styles.content}>
+          <Title name="dashboard">
+            <FiMessageSquare size={25} />
+          </Title>
+        </div>
+
+        {calls.length === 0 ? (
+          <section
+            className={`
+          texte
+          ${styles.container}  
+           ${styles.dashboard}`}
+          >
+            <span> Nenhum Chamado Registrado...</span>
+
+            <Link to="/new" className={styles.new}>
+              <FiPlus size={25} color="#fff" />
+              Novo Chamado
+            </Link>
+          </section>
+        ) : (
+          <>
+            {" "}
+            <Link to="/new" className={styles.new}>
+              <FiPlus size={25} color="#fff" />
+              Novo Chamado
+            </Link>{" "}
+          </>
+        )}
 
         <button onClick={() => signOut()}>Sair</button>
       </div>
